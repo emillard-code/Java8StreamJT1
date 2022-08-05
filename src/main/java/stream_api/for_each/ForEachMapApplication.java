81d47c5f45346851aa6d7251611b97d169c54a4f
Emerson Millard <emillard.code@gmail.com>
2022-08-05 12:00:00 -0700

    Added functionality.

--- src/main/java/stream_api/for_each/ForEachMapApplication.java
+++ src/main/java/stream_api/for_each/ForEachMapApplication.java
@@ -1,4 +1,23 @@
 package stream_api.for_each;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ForEachMapApplication {
+
+    public static void main( String[] args ) {
+
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        map.put(4, "Four");
+        map.put(5, "Five");
+
+        map.forEach((key, value) -> System.out.println(key + ": " + value));
+        map.entrySet().forEach(i -> System.out.println(i));
+
+    }
+
 }
