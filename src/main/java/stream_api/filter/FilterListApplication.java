f22d54b65b9adce4209e31a10cc0149326f050b9
Emerson Millard <emillard.code@gmail.com>
2022-08-05 12:00:00 -0700

    Added functionality.

--- src/main/java/stream_api/filter/FilterListApplication.java
+++ src/main/java/stream_api/filter/FilterListApplication.java
@@ -1,4 +1,22 @@
 package stream_api.filter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilterListApplication {
+
+    public static void main( String[] args ) {
+
+        List<String> list = new ArrayList<>();
+
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Five");
+
+        list.stream().filter(t -> t.startsWith("F")).forEach(i -> System.out.println(i));
+
+    }
+
 }
--- src/main/java/stream_api/for_each/ForEachListApplication.java
+++ src/main/java/stream_api/for_each/ForEachListApplication.java
@@ -8,6 +8,7 @@ public class ForEachListApplication {
     public static void main( String[] args ) {
 
         List<String> list = new ArrayList<>();
+
         list.add("One");
         list.add("Two");
         list.add("Three");
