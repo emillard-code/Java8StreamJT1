761d7d36722f17bb085edc4c273fb18d3727b8b8
Emerson Millard <emillard.code@gmail.com>
2022-08-05 12:00:00 -0700

    Added functionality.

--- src/main/java/stream_api/for_each/ForEachApplication.java
+++ src/main/java/stream_api/for_each/ForEachApplication.java
@@ -1,4 +1,21 @@
 package stream_api.for_each;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ForEachApplication {
+
+    public static void main( String[] args ) {
+
+        List<String> list = new ArrayList<>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Five");
+
+        list.stream().forEach(i -> System.out.println(i));
+
+    }
+
 }
