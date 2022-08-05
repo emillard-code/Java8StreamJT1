fb3213db195dbd7ce098a5090bf8cc90d0f51b9a
Emerson Millard <emillard.code@gmail.com>
2022-08-05 12:00:00 -0700

    Added functionality.

--- src/main/java/stream_api/filter/FilterMapApplication.java
+++ src/main/java/stream_api/filter/FilterMapApplication.java
@@ -1,4 +1,22 @@
 package stream_api.filter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FilterMapApplication {
+
+    public static void main( String[] args ) {
+
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        map.put(4, "Four");
+        map.put(5, "Five");
+
+        map.entrySet().stream().filter(i -> i.getKey() % 2 == 0).forEach(i -> System.out.println(i));
+
+    }
+
 }
