55f6162807723c5140274808cbb538b2a410c10a
Emerson Millard <emillard.code@gmail.com>
2022-08-06 12:00:00 -0700

    Added functionality.

--- src/main/java/sample_project/Employee.java
+++ src/main/java/sample_project/Employee.java
@@ -1,4 +1,24 @@
 package sample_project;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+
+    private int id;
+    private String name;
+    private String dept;
+    private long salary;
+
+    @Override
+    public String toString() {
+
+        return "Employee [id = " + id + ", name = " + name + ", dept = " + dept + ", salary = " + salary + "]";
+
+    }
+
 }
