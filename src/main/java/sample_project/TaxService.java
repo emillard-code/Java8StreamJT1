612a24339a4423448bb8c61198b7a3e93b00984b
Emerson Millard <emillard.code@gmail.com>
2022-08-06 12:00:00 -0700

    Minor modifications.

--- src/main/java/sample_project/TaxService.java
+++ src/main/java/sample_project/TaxService.java
@@ -5,6 +5,12 @@ import java.util.stream.Collectors;
 
 public class TaxService {
 
+    public static void main(String[] args) {
+
+        System.out.println(evaluateTaxUsers("tax"));
+
+    }
+
     public static List<Employee> evaluateTaxUsers(String input) {
 
         // Returns employees that make more than 500,000 a year when "tax" is the argument.
@@ -12,12 +18,6 @@ public class TaxService {
         return (input.equalsIgnoreCase("tax"))
                 ? Database.getEmployees().stream().filter(emp -> emp.getSalary() > 500000).collect(Collectors.toList())
                 : Database.getEmployees().stream().filter(emp -> emp.getSalary() <= 500000).collect(Collectors.toList());
-
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println(evaluateTaxUsers("tax"));
 
     }
 
