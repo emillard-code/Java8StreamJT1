fbce55be1778451577e7403e15dd55e348410895
Emerson Millard <emillard.code@gmail.com>
2022-08-06 12:00:00 -0700

    Added functionality.

--- src/main/java/sample_project/TaxService.java
+++ src/main/java/sample_project/TaxService.java
@@ -1,4 +1,24 @@
 package sample_project;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TaxService {
+
+    public static List<Employee> evaluateTaxUsers(String input) {
+
+        return (input.equalsIgnoreCase("tax"))
+
+                ? Database.getEmployees().stream().filter(emp -> emp.getSalary() > 500000).collect(Collectors.toList())
+                : Database.getEmployees().stream().filter(emp -> emp.getSalary() <= 500000)
+                .collect(Collectors.toList());
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(evaluateTaxUsers("tax"));
+
+    }
+
 }
